4aec7b0743dda2998a064ea14a2ecb5b3a27f828
Idowu Segun <segunidowu682@gmail.com>
2023-12-14 04:11:24 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,7 +4,6 @@
  * return- success
  */
 int main(void)
-{
-	puts("Programming is like building a multilingual puzzle\n");
+{	puts("\"Programming is like building a multilingual puzzle");
 	return 0;
 }
