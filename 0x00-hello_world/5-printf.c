9c799df3876767cc951a872a289ef37db985ef32
Idowu Segun <segunidowu682@gmail.com>
2023-12-14 04:17:23 -0800

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/*
+ * main- print out the function with proper grammar, but the outcome is a piece of art,
+ * return- success
+ */
+int main()
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
