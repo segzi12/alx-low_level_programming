187f7e0df16d3114464618cf28b99145f48f3cc2
Idowu Segun <segunidowu682@gmail.com>
2023-12-16 12:18:36 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,7 +4,7 @@
  * Description - Entry point of the program. puts the function
  * return - success
  */
-void main(void)
+int main(void)
 {	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
