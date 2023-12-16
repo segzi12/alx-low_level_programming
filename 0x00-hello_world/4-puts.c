fa1d3b1491036f0163e966cce60227117ba27d78
Idowu Segun <segunidowu682@gmail.com>
2023-12-16 12:44:35 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
+
 /**
  * main - puts "Programming is like building a multilingual puzzle
  * Description - Entry point of the program. puts the function
- * return - success
+ * Return: Returns 0
  */
+
 int main(void)
 {	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
