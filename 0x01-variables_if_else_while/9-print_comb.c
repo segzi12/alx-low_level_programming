ad0903af0935ba2fd12170b4879ddeb67dfde929
Idowu Segun <segunidowu682@gmail.com>
2023-12-18 14:51:10 -0800

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - Entry point of the program
+ * Return: Return 0
+ * Description: print all possible combination of single digit
+ *numbers, seperated by comma
+ */
+int main(void)
+{
+	int n = 0;
+
+	while (n < 10)
+	{
+		putchar('0' + n);
+		n++;
+		if (n <= 9)
+		{
+		putchar(',');
+		}
+	}
+	putchar('$');
+	putchar('\n');
+	return (0);
+}
