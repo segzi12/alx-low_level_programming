1325e208ee2c8f89cbb5a9380c77f06a1dfac239
Idowu Segun <segunidowu682@gmail.com>
2023-12-20 20:57:08 -0800

    add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * add - Entry point of the program
+ * Return: Return result
+ * @num1: The first integer parameter
+ * @num2: The second integer parameter
+ * Description: print out the addition of two numbers
+ */
+int add(int num1, int num2)
+{
+	int result = num1 + num2;
+
+	return (result);
+}
