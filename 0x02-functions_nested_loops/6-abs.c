6957d040d62bd7c344aa71614245b6e40b060da2
Idowu Segun <segunidowu682@gmail.com>
2023-12-20 19:35:52 -0800

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+  * _abs - Entry point of the program
+  * @n: The integer parameter
+  * Return: Return n
+  * Description: print the absolute of an integer
+  */
+int _abs(int n)
+{
+	n = abs(n);
+	return (n);
+}
