799f64f74cc584ea4f86ac433bc684cfe45411bd
Idowu Segun <segunidowu682@gmail.com>
2023-12-20 17:50:33 -0800

    islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <ctype.h>
+/**
+ * _islower - Entry point of the program that checks integer c
+ * integer c: the acting parameter
+ * Return: Return 1, if lower case and 0 if otherwise
+ * Description: This program takes parameter c as an integer and check
+ *if lowercase or not
+ */
+int _islower(int c)
+{
+	if (c >= 97 && c <= 123)
+	{
+		return (1);
+	}
+
+	else
+	{
+		return (0);
+	}
+}
